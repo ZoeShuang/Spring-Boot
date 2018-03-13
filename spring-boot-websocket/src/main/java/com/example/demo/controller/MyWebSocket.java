42904972fea5a6f8c37cf297662ebc92207b9310
shuang <zhushuangss@163.com>
2018-03-13 14:41:15 +0800

    spring-boot-websocket 实现群聊+单聊

--- spring-boot-websocket/src/main/java/com/example/demo/controller/MyWebSocket.java
+++ spring-boot-websocket/src/main/java/com/example/demo/controller/MyWebSocket.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.websocket.OnClose;
-import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -12,70 +14,106 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import domain.SocketMsg;
+
 
 @ServerEndpoint(value="/websocket/{nickname}")
 @Component
 public class MyWebSocket {
-	//用来存放每个客户端对应的MyWebSocket对象。  
-    private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<MyWebSocket>();  
-   
-    //与某个客户端的连接会话，需要通过它来给客户端发送数据  
-    private Session session;  
-    
-    private String nickname;
-   
-    /** 
-     * 连接建立成功调用的方法 
-     */  
-    @OnOpen  
-    public void onOpen(Session session,@PathParam("nickname") String nickname) {  
-        this.session = session;  
-        this.nickname=nickname;
-        webSocketSet.add(this);     //加入set中  
-        System.out.println("有新连接加入！当前在线人数为" + webSocketSet.size());  
-        this.session.getAsyncRemote().sendText("恭喜您成功连接上WebSocket-->当前在线人数为："+webSocketSet.size());  
-    }  
-   
-    /** 
-     * 连接关闭调用的方法 
-     */  
-    @OnClose  
-    public void onClose() {  
-        webSocketSet.remove(this);  //从set中删除  
-        System.out.println("有一连接关闭！当前在线人数为" + webSocketSet.size());  
-    }  
-   
-    /** 
-     * 收到客户端消息后调用的方法 
-     * 
-     * @param message 客户端发送过来的消息*/  
-    @OnMessage  
-    public void onMessage(String message, Session session) {  
-        System.out.println("来自客户端的消息:" + message);  
-   
-        //群发消息  
-        broadcast(message);  
-    }  
-   
-    /** 
-     * 发生错误时调用 
-     * 
-     */  
-    @OnError  
-    public void onError(Session session, Throwable error) {  
-        System.out.println("发生错误");  
-        error.printStackTrace();  
-    }  
-   
-    /** 
-     * 群发自定义消息 
-     * */  
-    public  void broadcast(String message){ 
-        for (MyWebSocket item : webSocketSet) {  
-               //同步异步说明参考：http://blog.csdn.net/who_is_xiaoming/article/details/53287691  
-               //this.session.getBasicRemote().sendText(message);  
-               item.session.getAsyncRemote().sendText("["+nickname+"]:"+message);//异步发送消息.  
-        }  
-    }  
+	//用来存储每个客户端对应的MyWebSocket对象.
+		private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<MyWebSocket>();
+		//用来记录sessionId和该session之间的绑定关系.
+		private static Map<String,Session> map = new HashMap<String,Session>();
+		
+		private Session session;//当前会话的session.
+		private String nickname;//昵称.
+		
+		/**
+		 * 成功建立连接调用的方法.
+		 */
+		@OnOpen
+		public void onOpen(Session session,@PathParam("nickname") String nickname){
+			this.session = session;
+			this.nickname = nickname;
+			map.put(session.getId(), session);
+			webSocketSet.add(this);//加入set中.
+			this.session.getAsyncRemote().sendText(nickname+"上线了,（我的频道号是"+session.getId()+"）");
+		}
+		
+		/**
+		 * 连接关闭调用的方法.
+		 */
+		@OnClose
+		public void onClose(Session session){
+			webSocketSet.remove(this);//从set中移除.
+			map.remove(session.getId());
+		}
+		
+		/**
+		 * 收到客户端消息后调用的方法.
+		 */
+		@OnMessage
+		public void onMessage(String message,Session session,@PathParam("nickname") String nickname){
+			
+			//message 不是普通的string ，而是我们定义的SocketMsg json字符串.
+			try {
+				SocketMsg socketMsg = new ObjectMapper().readValue(message, SocketMsg.class);
+				
+
+				//单聊.
+				if(socketMsg.getType() == 1){
+					
+					//单聊：需要找到发送者和接受者即可.
+					socketMsg.setFromUser(session.getId());//发送者.
+					//socketMsg.setToUser(toUser);//这个是由客户端进行设置.
+					Session fromSession = map.get(socketMsg.getFromUser());
+					Session toSession = map.get(socketMsg.getToUser());
+					if(toSession != null){
+						//发送消息.
+						fromSession.getAsyncRemote().sendText(nickname+"："+socketMsg.getMsg());
+						toSession.getAsyncRemote().sendText(nickname+"："+socketMsg.getMsg());
+					}else{
+						fromSession.getAsyncRemote().sendText("系统消息：对方不在线或者您输入的频道号有误");
+					}
+				}else {
+					//群发给每个客户端.
+					broadcast(socketMsg,nickname);
+				}
+				
+			} catch (JsonParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		/**
+		 * 发生错误时调用.
+		 */
+		public void onError(Session session,Throwable error){
+			System.out.println("发生错误");
+			error.printStackTrace();
+		}
+		
+		/**
+		 * 群发的方法.
+		 * @param message
+		 */
+		private void broadcast(SocketMsg socketMsg ,String nickname){
+			for(MyWebSocket item:webSocketSet){
+				//发送消息.
+				item.session.getAsyncRemote().sendText(nickname+"："+socketMsg.getMsg());
+			}
+		}
+		
 	
 }
